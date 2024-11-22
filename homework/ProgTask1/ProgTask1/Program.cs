10a46d442a449a7af026c7fedd1db8e6333a82bf
Kristopher Žuravljov <opilane@HP-CZC4247HK0>
2024-11-22 14:49:17 +0200

    ok

---
+++ homework/ProgTask1/ProgTask1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace ProgTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string uservalue = Console.ReadLine();
+            string uservalue2 = Console.ReadLine();
+
+
+            
+           int num1 = int.Parse(uservalue);
+            int num2 = int.Parse(uservalue2);
+            Console.WriteLine(num1 + num2);
+                        
+        }
+
+    }
+    internal class Calc {
+
+        static void Main(string[] args)
+        {
+            var Programm = new Calc();
+
+            Console.WriteLine("Starting Calculator");
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
