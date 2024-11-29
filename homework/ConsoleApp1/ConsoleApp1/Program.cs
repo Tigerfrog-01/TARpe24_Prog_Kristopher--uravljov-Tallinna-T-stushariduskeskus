1c158ebf4f52da77d74fc197f847e3103db48613
Tigerfrog <kristopherzuravljov005@gmail.com>
2024-11-29 14:43:56 +0200

    Napoleon is back

---
+++ homework/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.Design;
+using System.Linq.Expressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to quiz with 3 questions");
+            Console.WriteLine("Number 1: Wich answer is true about Napoleon Bonaparte");
+            Console.WriteLine("A: He was French");
+            Console.WriteLine("B: He was Italian");
+            Console.WriteLine("C: He was German");
+            Console.WriteLine("--------------------------------------------------------------------------");          
+          
+
+            string input = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------------------------------------");
+            if (input == "2")
+            {
+                Console.WriteLine("You are indeed correct, even if he borned in Corsica he had Italian roots!");
+            }
+
+            else
+            {
+                Console.WriteLine("you are not correct, 2 is Valid answer!!!");
+               
+                
+
+            }
+
+
+
+
+            Console.WriteLine("--------------------------------------------------------------------------");
+
+
+        }
+
+    }
+}
+
+        
+
+
+
+
+
+
+
+
+
---
+++ homework/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float num = 0.7;
+
+            if (num == 0.7);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
