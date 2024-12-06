2a66fabfdbc40266a950355ade74e050c9d0ae56
opilane <opilane@HP-CZC4247HK0>
2024-12-06 15:20:15 +0200

    PROG1 DONE

--- homework/ProgTask1/ProgTask1/Program.cs
+++ homework/ProgTask1/ProgTask1/Program.cs
@@ -9,19 +9,24 @@ namespace ProgTask1
     {
 static void Main(string[] args)
         {
-            Console.WriteLine("Insert 10 10 for skip");
-            Console.WriteLine("Starting Combining");
-            string uservalue = Console.ReadLine();
-            string uservalue2 = Console.ReadLine();
 
 
-            
-           int num1 = int.Parse(uservalue);
-            int num2 = int.Parse(uservalue2);
-            Console.WriteLine(num1 + num2);
-            var Programm = new Calc();
-            Programm.Run();
-           
+            var calculator = new Calc();
+            var quizGame = new Quiz();
+            var TrueorFalse = new TrueFalse();
+            var Floater = new Floater();
+            var Age = new Age();
+
+            Console.WriteLine("Starting Calculator:");
+            calculator.Run();
+            Console.WriteLine("\nStarting Quiz Game:");
+            quizGame.Run();
+            Console.WriteLine("\nStarting True or False:");
+            TrueorFalse.Run();
+            Console.WriteLine("\nStarting Floating:");
+            Floater.Run();
+            Console.WriteLine("\nAge checking:");
+            Age.Run();
         }
 
     }
@@ -30,35 +35,225 @@ static void Main(string[] args)
         public void Run()
         {
             var Programm = new Calc();
+            string number = Console.ReadLine();
+            string number1 = Console.ReadLine();
+            int input1 = int.Parse(number);
+            int input2 = int.Parse(number1);
+            int value = int.Parse(number);
+            int value2 = int.Parse(number1);
+            //Console.WriteLine($"Sum of 2 numbers: {value2 + value}");
+            Add2Numbers(value2,value);
+            Console.WriteLine($"Sum of 2 numbers: {value2 - value}");
+            Console.WriteLine($"Sum of 2 numbers: {value2 / value}");
+            Console.WriteLine($"Sum of 2 numbers: {value2 * value}");
 
-            Console.WriteLine("Starting Divding");
-             string uservalue3 = Console.ReadLine();
-            string uservalue4 = Console.ReadLine();
 
 
-            int num1 = int.Parse(uservalue3);
-            int num2 = int.Parse(uservalue4);          
-            Console.WriteLine(num1 / num2);
 
-            Console.WriteLine("Starting Multiplying");
-            string uservalue6 = Console.ReadLine();
-            string uservalue7 = Console.ReadLine();
-            int num3 = int.Parse(uservalue6);
-            int num4 = int.Parse(uservalue7);
-            Console.WriteLine(num3 * num4);        
 
-            Console.WriteLine("Starting Substraction");
-            string uservalue8 = Console.ReadLine();
-            string uservalue9 = Console.ReadLine();
-            int num5 = int.Parse(uservalue8);
-            int num6 = int.Parse(uservalue9);
-            Console.WriteLine(num5 - num6);
+
+
+
+
+        }
+    
+    public void Add2Numbers(int number1, int number2)
+    {
+        // Implement the logic for adding 2 numbers here and print
+        Console.WriteLine($"Sum of 2 numbers: {number1 + number2}");
+    }
+}
+    }
+
+internal class Quiz
+{
+
+    public void Run()
+    {
+
+        Console.WriteLine("Welcome to quiz with 3 questions");
+            Console.WriteLine("Number 1: Wich answer is true about Napoleon Bonaparte");
+            Console.WriteLine("A: He was French");
+            Console.WriteLine("B: He was Italian");
+            Console.WriteLine("C: He was German");
+            Console.WriteLine("--------------------------------------------------------------------------");          
+          
+
+            string input = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------------------------------------");
+            if (input == "b")           
+            {
+                Console.WriteLine("You are indeed correct, even if he borned in Corsica he had Italian roots!");
+            }
+
+            else
+            {
+                Console.WriteLine("you are not correct, B is Valid answer!!!");
+               
+                
+
+            }
+            Console.WriteLine("--------------------------------------------------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
+internal class TrueFalse
+{
+
+    public void Run()
+    {
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Console.WriteLine("True or False Quiz");
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Console.WriteLine("Platypus is mammal");
+        Console.WriteLine("--------------------------------------------------------------------------");
+        string input = Console.ReadLine();
+        Console.WriteLine("--------------------------------------------------------------------------");
+        int value = 0;
+        int value1 = 0;
+        int value2 = value1;
+        int value3 = value2;
+
+        if (input == "true")
+
+        {
+            Console.WriteLine("You are correct, Fun fact even if it lays eggs its still mammal");
+            value1 = value + 1;
+
+
 
         }
 
+        else
+        {
+            Console.WriteLine("you are not correct, answer was true!");
+
+
+
+        }
+
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Console.WriteLine("Next Question, Was Petersburg named after Peter the first");
+        Console.WriteLine("--------------------------------------------------------------------------");
+        string input1 = Console.ReadLine();
+        Console.WriteLine("--------------------------------------------------------------------------");
+        if (input1 == "true")
+        {
+            Console.WriteLine("You are correct, Peter the first wanted its own capital as its name");
+            value2 = value1 + 1;
+
+        }
+
+        else
+        {
+            Console.WriteLine("you are not correct, answer was true!");
+
+
+
+        }
+
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Console.WriteLine("Next Question, Was Victoria the first, queen of England  170cm tall");
+        Console.WriteLine("--------------------------------------------------------------------------");
+        string input2 = Console.ReadLine();
+        Console.WriteLine("--------------------------------------------------------------------------");
+        if (input2 == "false")
+        {
+            Console.WriteLine("You are correct, Victoria wasnt 170cm tall but 147cm tall");
+            value3 = value2 + 1;
+        }
+
+        else
+        {
+            Console.WriteLine("you are not correct, answer was true!");
+
+
+        }
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Console.WriteLine("Total score is:");
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Console.WriteLine(value3);
     }
+
 }
+internal class Floater
+{
+
+    public void Run()
+    {
+
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Console.WriteLine("Quiz on Float!");
+        Console.WriteLine("Wich one is Float");
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Console.WriteLine("A: a");
+        Console.WriteLine("B: 4,5");
+        Console.WriteLine("C: 1");
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Console.WriteLine("If it says single you are correct!");
+        string tree = Console.ReadLine();
+        float leaf = float.Parse(tree);       
+        Console.WriteLine(leaf);
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Console.WriteLine(leaf.GetType().Name);
+        Console.WriteLine("--------------------------------------------------------------------------");
+
+
+    }
+
+}
+internal class Age
+{
+    public void Run()
+    {
+
+        Console.WriteLine("--------------------------------------------------------------------------");
+        string tree = Console.ReadLine();
+        Console.WriteLine("--------------------------------------------------------------------------");
+        int value1 = int.Parse(tree);
+
+            if (value1< 13)
+            {
+                Console.WriteLine("Alla 13-aastased: \"Naudi oma lapsepõlve!\"");
+            }
+            else if (value1 >= 12 && value1 <= 19)
+{
+    Console.WriteLine("13 kuni 19 aastat: \"Need on mõned mässulised teismeliseaastad!\"");
+}
+else if (value1 < 65)
+{
+    Console.WriteLine("Aastaid rohkem kui 19: \"Tere tulemast täiskasvanud elu!\"");
+}
+else
+{
+    Console.WriteLine("Haara kinni oma kuldaastate tarkusest!");
+}
+        Console.WriteLine("--------------------------------------------------------------------------");
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
