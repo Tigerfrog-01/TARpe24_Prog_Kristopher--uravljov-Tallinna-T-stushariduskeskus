dfe222d775df91dc74522ec9550f028d2c06f9b7
tiger <krissuzuravljov@gmail.com>
2025-01-16 11:41:03 +0200

    done

---
+++ homework/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Runtime.ConstrainedExecution;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> lst = new List<string>();
+
+
+            lst.Add(Console.ReadLine());
+            lst.Add(Console.ReadLine());
+            lst.Add(Console.ReadLine());
+            lst.Add(Console.ReadLine());
+            lst.Reverse();           
+            Console.WriteLine("-----------------------------------------");
+
+
+            foreach (string word in lst)
+            {
+
+
+                Console.WriteLine(word);
+
+            }
+
+            List<string> lst1 = new List<string>();
+
+            Console.WriteLine("-----------------------------------------");
+            lst1.Add(Console.ReadLine());
+            lst1.Add(Console.ReadLine());
+            lst1.Add(Console.ReadLine());
+            lst1.Add(Console.ReadLine());
+            lst1.Sort();
+            Console.WriteLine("-----------------------------------------");
+
+
+            foreach (string word in lst1)
+            {
+
+
+                Console.WriteLine(word);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        }
+    }
+
