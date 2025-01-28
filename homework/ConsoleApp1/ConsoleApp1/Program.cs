609276a166b6857951172d696edd5c7d9acedf86
opilane <opilane@HP-CZC1339D23>
2025-01-28 10:47:24 +0200

    done

---
+++ homework/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double principal = 2000;
+            double interestRate = 2.1 / 100;
+            int years = 1;
+
+            double finalAmount = FindCompoundInterest(principal, interestRate, years);
+            Console.WriteLine($"The final amount after {years} years is {finalAmount}");
+        }
+
+        static double
+        FindCompoundInterest(double principal, double interestRate, int years)
+
+
+        {
+
+            for (int i = 0; i < years; i++)
+            {
+
+                double interest = principal * interestRate;
+                principal += interest;
+
+            }
+
+            return principal;
+
+        }
+
+    }
+}
+
+        
+    
+     
+
+
+
---
+++ homework/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> list = new List<string>();
+            using (StreamReader reader = new StreamReader("file.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    list.Add(line); 
+                    Console.WriteLine(line); 
+                }
+                Console.WriteLine("--------------------------------");
+            }
+            List<string> list1 = new List<string>();
+            using (StreamReader reader = new StreamReader("half.txt"))
+            {
+                string line1;
+                while ((line1 = reader.ReadLine()) != null)
+                {
+                    list.Add(line1);
+                    Console.WriteLine(line1);
+                }
+            }
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
