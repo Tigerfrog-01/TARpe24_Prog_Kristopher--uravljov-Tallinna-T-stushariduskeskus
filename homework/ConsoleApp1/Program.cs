d1c8c64c6e42dc1ea60cc05135966f9a6630f42c
yes <no>
2025-02-27 12:28:28 +0200

    its done 10

--- homework/ConsoleApp1/Program.cs
+++ homework/ConsoleApp1/Program.cs
@@ -1,66 +1,105 @@
-﻿using System.Collections.Generic;
-using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
-using System.Runtime.ConstrainedExecution;
-using System.Transactions;
+﻿
+using System.ComponentModel.Design;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+
+
+        public class Circle
         {
 
-            var CircleCalc = new Circle();
-          
-         
-            Console.WriteLine("Starting CircleCalc:");
-            CircleCalc.Circle1();
-            
+            private double radius;
+            private double diameter;
 
 
+            public Circle()
+            {
+
+                radius = 10;
+                diameter = 5;
+            }
+
+            public Circle(double radius)
+            {
+                SetRadius(radius);
+            }
+
+            public void PrintInfo()
+            {
+                if (radius == 0 && diameter == 0)
+                {
+                    Console.WriteLine("Values are not set!");
+                }
+                else
+                {
+                    Console.WriteLine($"Circle radius is {radius} and circle diameter is {diameter}");
+                }
+            }
 
 
+            public void SetRadius(double radius)
+            {
+                this.radius = radius;
+                this.diameter = radius * 2;
+            }
 
 
+            public void SetDiameter(double diameter)
+            {
+                this.diameter = diameter;
+                this.radius = diameter / 2;
+            }
 
 
+            public void CalculateArea()
+            {
+                double area = Math.PI * Math.Pow(radius, 2);
+                Console.WriteLine($"Circle area is {Math.Round(area, 2)}");
+            }
 
 
+            public void CalculateCircumference()
+            {
+                double circumference = 2 * Math.PI * radius;
+                Console.WriteLine($"Circle circumference is {Math.Round(circumference, 2)}");
+            }
+        }
+
+        class Final
+        {
+            static void Main(string[] args)
+            {
+
+                Circle circle1 = new Circle();
+                circle1.PrintInfo();
+
+
+                circle1.SetRadius(5);
+
+
+                circle1.CalculateArea();
+                circle1.CalculateCircumference();
+
+
+                Circle circle2 = new Circle(3);
+                circle2.PrintInfo();
+
+
+                circle2.SetDiameter(10);
+
+                circle2.CalculateArea();
+                circle2.CalculateCircumference();
+            }
         }
     }
+
 }
 
-internal class Circle
-{
-
-    public void Circle1()
-    {
-
-        double diameter = 72;
-        double radius = diameter / 2;
-        double area = 3.14 * radius;
-        Console.WriteLine(area);
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-}
---
+++ homework/ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double money = 0;
+            Console.WriteLine("Total money:" + money);     
+            Console.WriteLine("Card is Swedbank");
+            string number = "50704579";
+            if (number.Length == 8)
+
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+
+                Console.WriteLine("invalid amount, lenght is 8, above or below is invalid");
+            }
+
+            double addmoney = 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
