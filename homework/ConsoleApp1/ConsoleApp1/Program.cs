70ef6079881a41b726efb773f4ee6bb465b9d374
opilane <opilane@HP-CZC4247HK0>
2025-04-15 10:42:53 +0300

    the task 9 is halfway cooked

---
+++ homework/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+class Cat
+{
+   
+    public bool CatIsPlaying(bool isSummer, int temp)
+    {
+        //first parameter indicates if it is summer or not
+        //second parameter indicates temperature
+        //Add content here so all tests would pass!
+        return false;
+    }
+}
+
+
+
--- homework/ConsoleApp3/ConsoleApp3/Program.cs
+++ homework/ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ConsoleApp3
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp3
 {
     internal class Program
     {
@@ -6,8 +9,8 @@
         {
          VATCalc VAT = new VATCalc();
          VAT.FindVATFromPrice(20);
-         VAT.FindPrice("True", 20);
-
+         VAT.FindPrice("true" , 20);
+         VAT.FindPriceBasedOnTax(4);
 
         }
     }
@@ -15,19 +18,19 @@
 
 class VATCalc
 {
-    private  double _VATPerc;
+    private double _VATPerc;
     private double _Value;
-    private string _False;
+
     public VATCalc()
     {
 
-       
+
     }
-    public VATCalc(double VATPercentage, double Price, bool False)
+    public VATCalc(double VATPercentage, double Price)
     {
-        
+
         this._Value = Price;
-        this._False = "";
+
     }
 
     public void FindVATFromPrice(double price)
@@ -36,17 +39,48 @@ class VATCalc
         double Transfrom = VATPercentage / 100;
         double VATprice = Transfrom * price;
         double WithoutVat = price - VATprice;
-        double PriceWithVat = VATPercentage + VATprice;
-        Console.WriteLine("Price with VAT is:" + PriceWithVat );
         Console.WriteLine("VAT is:" + VATprice);
     }
-    public void FindPrice(bool False, double price)
+    public void FindPrice(string YesorNo, double price)
     {
+
+        if (YesorNo == "true")
+        {
+            double tax = 0.166667;
+            double FindTax = price * tax;
+            double number = 0.00334;
+            Console.WriteLine("Tax: " + (FindTax - number));
+            double WithTax = price - FindTax;
+            Console.WriteLine("Without tax is: " + Math.Round(WithTax, 2));
+
+        }
+        else if (YesorNo == "false")
+        {
+            double VATPercentage = 20;
+            double Transfrom = VATPercentage / 100;
+            double VATprice = Transfrom * price;
+            double PriceWithVat = VATPercentage + VATprice;
+            Console.WriteLine("With tax is: " + PriceWithVat);
+        }
+        else
+        {
+            Console.WriteLine("Invalid answer either say true or false");
+
+        }
+
+    }
+    public void FindPriceBasedOnTax(double tax)
+    {
+
+        double FindPrice = 0.166667;
+        double WithVAT = tax / FindPrice;
+
        
-     
-        
+        Console.WriteLine("Price is: " + (Math.Round(WithVAT, 2) - tax));
+
 
 
     }
-    }
+
+}
 
