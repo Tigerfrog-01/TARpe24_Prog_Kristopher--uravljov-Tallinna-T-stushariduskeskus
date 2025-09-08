1e5126344c981450274783fc9cacbe23014841ec
Opilane TTHK <kristopherzuravljov005@gmail.com>
2025-09-08 15:39:55 +0300

    FREEDOM
    FREEDOM

---
+++ TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Data;
+using TallinnaRakenduslikKolledz.Models;
+
+namespace TallinnaRakenduslikKolledz.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+        public StudentsController(SchoolContext context)
+        {
+
+            _context = context;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create([Bind
+        ("ID,LastName,FirstName,EnrollmentDate,Nationality")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return View(student);
+        }
+
+
+
+    }
+}
