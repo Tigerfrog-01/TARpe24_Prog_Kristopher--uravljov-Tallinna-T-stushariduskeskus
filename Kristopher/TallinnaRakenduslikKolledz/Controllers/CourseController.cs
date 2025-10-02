08f034b822b215b836582da10ff63dfed8716853
opilane <kristopherzuravljov005@gmail.com>
2025-10-02 16:12:43 +0300

    fixed?

---
+++ Kristopher/TallinnaRakenduslikKolledz/Controllers/CourseController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Data;
+using TallinnaRakenduslikKolledz.Models;
+
+namespace TallinnaRakenduslikKolledz.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly SchoolContext _context;
+        public CourseController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var courses = _context.Courses.Include(c => c.Department)
+                .AsNoTracking();
+
+            return View(courses);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+           // PopulateDepartmentsDropDownList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Course Course)
+        {
+            if (ModelState.IsValid)
+            {
+           
+            _context.Add(Course);
+            await _context.SaveChangesAsync();
+           // PopulateDepartmentsDropDownList(Course.DepartmentID);
+            }
+            return RedirectToAction("Index");
+            
+        }
+
+
+        [HttpGet]   
+        public async Task<IActionResult> Delete(int?id)
+        {
+            if (id == null || _context.Courses == null)
+            {
+                return NotFound();
+            }
+            var courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CourseID == id);
+            if (courses == null)
+            {
+                return NotFound();
+            }
+            return View(courses);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Deleteconfirmed(int id)
+        {
+            if (_context.Courses == null)
+            {
+                return NotFound();
+            }
+            var course = await _context.Courses.FindAsync(id);
+            if(course !=null)
+            {
+                _context.Courses.Remove(course);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction("index");
+        }
+
+    
+
+
+        private void PopulateDepartmentsDropDownList(object selectedDepartment = null)
+        {
+            var departmentsQuery = from d in _context.Departments
+                                   orderby d.Name
+                                   select d;
+            ViewBag.DepartmentID = new SelectList(departmentsQuery.AsNoTracking(), "DepartmentID", "Name", selectedDepartment);
+
+        }
+
+    }
+}
