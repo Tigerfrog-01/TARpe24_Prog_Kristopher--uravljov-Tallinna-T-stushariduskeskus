ab6a575ee1bfac730611f4bcb69195f0f15c7792
opilane <kristopherzuravljov005@gmail.com>
2025-10-02 15:45:10 +0300

    ok

---
+++ TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
@@ -0,0 +1,138 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Data;
+using TallinnaRakenduslikKolledz.Models;
+
+namespace TallinnaRakenduslikKolledz.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+        private object var_student;
+        private Student student;
+
+        public StudentsController(SchoolContext context)
+        {
+
+            _context = context;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+
+        public async Task<IActionResult> Create([Bind
+        ("ID,LastName,FirstName,EnrollmentDate,Nationality")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return View(student);
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(student);
+
+
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("index");
+
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Vaata(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+                
+            }
+
+            return View(student);
+
+
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == id);
+            
+            if (student == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(student);
+
+
+        }
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+
+
+        public async Task<IActionResult> EditConfirm([Bind
+        ("Id,LastName,FirstName,EnrollmentDate,Nationality")] Student student)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Students.Update(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return NotFound();
+           
+        }
+
+    }
+}
---
+++ TallinnaRakenduslikKolledz/Data/SchoolContext.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Models;
+
+namespace TallinnaRakenduslikKolledz.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+        //
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<CourseAssigment> CourseAssigments { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Students");
+            //
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<CourseAssigment>().ToTable("CourseAssignment");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            //
+            modelBuilder.Entity<Department>().ToTable("Department");
+
+
+        }
+        
+
+    }
+}
